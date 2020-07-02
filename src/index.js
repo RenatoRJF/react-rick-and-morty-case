a688b0c571427a05282a19beafc83a30e633c351
Renato Ferreira <renatoferreira@MacBook-Pro.domain_not_set.invalid>
2020-07-02 15:57:59 +0200

    Configure graphql client

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import client from "./services/api";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
