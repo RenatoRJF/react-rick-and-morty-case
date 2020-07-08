fd25f56d77f7a77ff568673ac1cab52a431d5ffd
Renato Ferreira <renatoferreira@MacBook-Pro.domain_not_set.invalid>
2020-07-08 21:59:48 +0200

    Implement characters search

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
 
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import client from "./services/api";
+
+import App from "./App";
 
 ReactDOM.render(
   <ApolloProvider client={client}>
