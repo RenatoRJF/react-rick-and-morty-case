94f9fec5e72a0f7e8b130f84c20f216d2b6a834a
Renato Ferreira <renatoferreira@MacBook-Pro.domain_not_set.invalid>
2020-07-09 09:27:43 +0200

    Add image to NotFound component

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,22 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 import { COLORS } from "../../styles/variables";
+import RickAndMorty from "../../images/rick-and-morty.png";
 
-const Text = styled.p({
-  fontSize: 50,
-  color: COLORS.white,
-  marginBottom: 20,
+const Container = styled.div({
+  textAlign: "center",
+  marginBottom: 50,
 });
+
+const Text = styled.p`
+  font-size: 40px;
+  color: ${COLORS.white};
+
+  @media screen and (min-width: 768px) {
+    font-size: 50px;
+  }
+`;
 
 const CodeNumber = styled.span({
   color: COLORS.bumblebee,
   fontSize: 50,
 });
 
+const Image = styled.img({
+  width: 250,
+});
+
 export default function NotFound() {
   return (
-    <Text>
-      <CodeNumber>404</CodeNumber> Not Found
-    </Text>
+    <Container>
+      <Image src={RickAndMorty} alt="rick and morty" />
+      <Text>
+        <CodeNumber>404</CodeNumber> Not Found
+      </Text>
+    </Container>
   );
 }
